3841cbafa9e5e0100bf6584dc74459455d7bb348
jun <dla3188@gmail.com>
2025-03-26 15:35:58 +0900

    lim

--- client/run_client.py
+++ client/run_client.py
@@ -3,7 +3,7 @@ import psutil
 from time import sleep
 
 TARGET_IP = "192.168.0.174"
-# TARGET_IP = "172.17.0.1"
+
 API_URL = f"http://{TARGET_IP}:8000/cpu-monitor"
 
 while True:
@@ -14,4 +14,4 @@ while True:
     response = requests.post(API_URL, json=payload)
 
     print(f"Server response: {response.status_code}")
-
+### asdasd
